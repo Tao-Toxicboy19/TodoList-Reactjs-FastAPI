cb2311723c8991432ba8d2cf407ac5ad401c38aa
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-08-01 01:07:59 +0700

    Add Checkbox

--- todolist-reactjs-fastapi/src/components/Pages/CreatePage/CreatePage.tsx
+++ todolist-reactjs-fastapi/src/components/Pages/CreatePage/CreatePage.tsx
@@ -11,7 +11,7 @@ export default function TodolistPage() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  const [checkboxStates, setCheckboxStates] = useState<{
+      const [checkboxStates, setCheckboxStates] = useState<{
     [id: string]: boolean;
   }>({});
 
